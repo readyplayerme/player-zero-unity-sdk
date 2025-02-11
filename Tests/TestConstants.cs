550b2b11000fd44dcb49817c7c74fc148c87e678
MaxAndreassenRPM <max@wolf3d.io>
2025-02-11 09:36:45 +0000

    fix: remove tests folder

--- Tests/TestConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace PlayerZero.Tests
-{
-    public class TestConstants
-    {
-        public const string ApplicationId = "665e05a50c62c921e5a6ab84";
-        public const string CharacterId = "667182f34806feb40dc3a41c";
-        public const string TemplateId = "665e05e758e847063761c985";
-    }
-}
