57a4e4d7cf0c8dd48b519afbdecb5f7c401c5a26
MaxAndreassenRPM <max@wolf3d.io>
2025-02-19 16:57:44 +0000

    feat: adds static player zero sdk

---
+++ Runtime/Sdk/PlayerZeroSdk.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using GLTFast;
+using PlayerZero.Api.V1;
+using UnityEngine;
+
+namespace PlayerZero.Runtime.Sdk
+{
+    public static class PlayerZeroSdk
+    {
+        private static CharacterApi _characterApi;
+
+        public static void Init()
+        {
+            if (_characterApi == null)
+                _characterApi = new CharacterApi();
+        }
+
+        public static async Task<GameObject> InstantiateAvatarAsync(
+            string avatarId,
+            string blueprintId,
+            Transform parent = null
+            )
+        {
+            Init();
+
+            var response = await _characterApi.FindByIdAsync(new CharacterFindByIdRequest()
+            {
+                Id = avatarId,
+            });
+			
+            var url = $"{response.Data.ModelUrl}?targetBlueprintId={blueprintId}";
+			
+            var gltf = new GltfImport();
+            if (!await gltf.Load(url))
+            {
+                Debug.LogError( $"Failed to load Player Zero Character" );
+            }
+
+            var playerZeroCharacterParent = new GameObject("PlayerZeroImportContainer");
+
+            await gltf.InstantiateSceneAsync(playerZeroCharacterParent.transform);
+            
+            var playerZeroCharacter = playerZeroCharacterParent.transform.GetChild(0).gameObject;
+            playerZeroCharacter.transform.parent = parent;
+
+            GameObject.Destroy(playerZeroCharacterParent);
+
+            return playerZeroCharacter;
+        }
+    }
+}
