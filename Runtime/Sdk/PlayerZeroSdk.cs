dd1f01005bf4bc20b380bf58069e5bc19059bf2b
MaxAndreassenRPM <max@wolf3d.io>
2025-02-21 12:49:30 +0000

    feat: adds username field for character

--- Runtime/Sdk/PlayerZeroSdk.cs
+++ Runtime/Sdk/PlayerZeroSdk.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 
 namespace PlayerZero.Runtime.Sdk
 {
+    public struct CharacterRequestConfig
+    {
+        public string AvatarId { get; set; }
+        
+        public string AvatarUrl { get; set; }
+        
+        public string BlueprintId { get; set; }
+        
+        public Transform Parent { get; set; }
+    }
+    
     public static class PlayerZeroSdk
     {
         private static CharacterApi _characterApi;
 
-        public static void Init()
+        private static void Init()
         {
             if (_characterApi == null)
                 _characterApi = new CharacterApi();
         }
-
-        public static async Task<GameObject> InstantiateAvatarAsync(
-            string avatarId,
-            string blueprintId,
-            Transform parent = null
-            )
+        
+        public static async Task<Character> GetAvatarMetadataAsync(
+            string avatarId
+        )
         {
             Init();
 
@@ -27,9 +36,34 @@ namespace PlayerZero.Runtime.Sdk
             {
                 Id = avatarId,
             });
-			
-            var url = $"{response.Data.ModelUrl}?targetBlueprintId={blueprintId}";
-			
+
+            return response.Data;
+        }
+
+        public static async Task<GameObject> InstantiateAvatarAsync(CharacterRequestConfig request)
+        {
+            if (string.IsNullOrEmpty(request.AvatarId) && string.IsNullOrEmpty(request.AvatarUrl))
+                Debug.LogError("One of either AvatarId or AvatarUrl must be provided.");
+            
+            if (!string.IsNullOrEmpty(request.AvatarId) && !string.IsNullOrEmpty(request.AvatarUrl))
+                Debug.LogError("Only one of either AvatarId or AvatarUrl must be provided.");
+            
+            Init();
+
+            string url;
+
+            if (!string.IsNullOrEmpty(request.AvatarUrl))
+            {
+                url = $"{request.AvatarUrl}?targetBlueprintId={request.BlueprintId}";
+            } else {
+                var response = await _characterApi.FindByIdAsync(new CharacterFindByIdRequest()
+                {
+                    Id = request.AvatarId,
+                });
+                
+                url = $"{response.Data.ModelUrl}?targetBlueprintId={request.BlueprintId}";
+            }
+
             var gltf = new GltfImport();
             if (!await gltf.Load(url))
             {
@@ -41,9 +75,12 @@ namespace PlayerZero.Runtime.Sdk
             await gltf.InstantiateSceneAsync(playerZeroCharacterParent.transform);
             
             var playerZeroCharacter = playerZeroCharacterParent.transform.GetChild(0).gameObject;
-            playerZeroCharacter.transform.parent = parent;
+            playerZeroCharacter.transform.parent = request.Parent;
 
             GameObject.Destroy(playerZeroCharacterParent);
+            
+            playerZeroCharacter.transform.localPosition = Vector3.zero;
+            playerZeroCharacter.transform.localEulerAngles = Vector3.zero;
 
             return playerZeroCharacter;
         }
