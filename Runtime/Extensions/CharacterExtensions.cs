bb7a6adc2e57f655b7f66f9847bdc6a4dca60aa5
Harrison Hough <harrisonhough4@gmail.com>
2025-07-09 07:53:02 +0300

    feat: add isMale check

---
+++ Runtime/Extensions/CharacterExtensions.cs
@@ -0,0 +1,17 @@
+﻿using PlayerZero.Api.V1;
+using UnityEngine;
+
+namespace PlayerZero
+{
+    public static class CharacterExtensions
+    {
+        private const string DEFAULT_FEMALE_BLUEPRINT_ID = "65e0a59ebba2f3e2a45841ff";
+        
+        public static bool IsMale(this Character character)
+        {
+            if (character != null) return character.BlueprintId != DEFAULT_FEMALE_BLUEPRINT_ID;
+            Debug.LogError("Character is null.");
+            return true;
+        }
+    }
+}
